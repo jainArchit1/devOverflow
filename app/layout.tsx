c68352135bc1a731211049b0fb0017656b12c1af
jainArchit1 <architsupper@gmail.com>
2024-06-21 09:49:53 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Nextjs 14",
+  description: "xyz",
+};
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <header>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+          <main>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
